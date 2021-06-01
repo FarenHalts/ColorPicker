276c0c1b59e86f99772f63aaf0f45aa0e682fba5
Lucas Garcia <47766322+FarenHalts@users.noreply.github.com>
2021-06-01 03:40:32 -0300

    Finishing color picker component

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <ColorPicker :initialSelectedColor="initialColor" :colorPickerOptions="selectableColors" />
+  </div>
+</template>
+
+<script>
+import ColorPicker from './components/ColorPicker.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ColorPicker,
+  },
+  data() {
+    return {
+      initialColor: {
+        name: 'black',
+        color: '#000000',
+      },
+      selectableColors: [
+        {
+          name: 'purple',
+          color: '#6f56f6',
+        },
+        {
+          name: 'green',
+          color: '#00e285',
+        },
+        {
+          name: 'orange',
+          color: '#ffb858',
+        },
+        {
+          name: 'hot pink',
+          color: '#ff1261',
+        },
+        {
+          name: 'baby pink',
+          color: '#ffa8c1',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Quicksand', sans-serif;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="square-picker">
+    <div
+        class="square-picker__exposure-color"
+        :style="`background-color: ${exposureColor.color || initialSelectedColor.color}`"
+      >
+      <div
+        class="square-picker__exposure-color__name"
+        :style="`color: ${exposureColor.color || initialSelectedColor.color}`"
+      >
+        {{ exposureColor.name || initialSelectedColor.name }}
+      </div>
+    </div>
+    <div class="square-picker__selectable">
+      <div v-for="(item, index) in colorPickerOptions" :key="index">
+        <div
+          class="square-picker__selectable__color"
+          :style="`background-color: ${item.color}`"
+          @click="changeColor(item)"
+        >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  props: {
+    initialSelectedColor: Object,
+    colorPickerOptions: Array,
+  },
+  data() {
+    return {
+      exposureColor: {
+        color: '',
+        name: '',
+      },
+    };
+  },
+
+  methods: {
+    changeColor(item) {
+      this.exposureColor = {
+        color: item.color,
+        name: item.name,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
